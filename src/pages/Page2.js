ded76cc3760e750571722c449274a1e6cc0b4151
Hattori-Takuma <hattori.ta@icloud.com>
2021-10-02 14:44:13 +0900

    じゃんけん判定

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -2,123 +2,80 @@ import React, { useState, useEffect } from 'react'
 
 const Page2 = () => {
 
-
-
-
-
-
   const [hand, setHand] = useState("");
+  const [cpuhand, setCpuhand] = useState("");
   const [count, setCount] = useState(0);
-
+  const [result, setResult] = useState("");
 
 
   useEffect(() => {
     console.log('useEffect が呼び出されました。')
-
   },
     [count]
   );
 
-
-
-
   const gu = () => {
     setHand("ぐー")
     setCount(count + 1)
-
     console.log("gu")
-
-
+    CPUdeside()
+    judge()
   };
 
   const choki = () => {
     setHand("ちょき")
     setCount(count + 1)
     console.log("choki")
-
-
-
+    CPUdeside()
+    judge()
   };
 
   const pa = () => {
     setHand("ぱー")
     setCount(count + 1)
     console.log("pa")
-
+    CPUdeside()
+    judge()
   };
 
 
+  const CPUdeside = () => {
+    const com = Math.floor(Math.random() * 3) + 1;
+    console.log(com)
 
-  let mainHand = '';
-  switch (hand) {
-    case "ぐー":
-      mainHand = 1;
-      break;
-    case "ちょき":
-      mainHand = 2;
-      break;
-    case "ぱー":
-      mainHand = 3;
-      break;
-    default:
+    // コンピュータの手の名前
+    switch (com) {
+      case 1:
+        setCpuhand("ぐー");
+        console.log("gu(cp)")
+        break;
+      case 2:
+        setCpuhand("ちょき");
+        console.log("choki(cp)")
+        break;
+      case 3:
+        setCpuhand("ぱー");
+        console.log("pa(cp)")
+        break;
+      default:
+    }
   }
 
+  const judge = () => {
 
+    if (hand === cpuhand) {
+      setResult("あいこ");
+    } else if ((cpuhand === "ぐー" && hand === "ぱー") || (cpuhand === "ちょき" && hand === "ぐー") || (cpuhand === "ぱー" && hand === "ちょき")) {
+      setResult("勝ちました");
+    } else if ((cpuhand === "ぐー" && hand === "ちょき") || (cpuhand === "ちょき" && hand === "ぱー") || (cpuhand === "ぱー" && hand === "ぐー")) {
+      setResult("負けました");
+    } else {
+      setResult('...')
+    }
+    console.log(cpuhand)
+    console.log(hand)
 
-
-
-  //const [Cpu, setCpu] = useState("");
-
-  //const random = Math.floor(Math.random() * 3)
-
-
-  const GU = 1;
-  const CHOKI = 2;
-  const PA = 3;
-
-
-
-
-  const com = Math.floor(Math.random() * 3) + 1;
-  console.log(com)
-
-
-  // コンピュータの手の名前
-  let comHandName = '';
-  switch (com) {
-
-    case GU:
-      comHandName = 'グー!!';
-      break;
-    case CHOKI:
-      comHandName = 'チョキ!!';
-      break;
-    case PA:
-      comHandName = 'パー!!';
-      break;
-    default:
-  }console.log(comHandName)
-
-
-
-  let result = '';
-  if (mainHand === com) {
-    result = '結果はあいこでした。';
-  } else if ((com === GU && mainHand === 3) || (com === CHOKI && mainHand === 1) || (com === PA && mainHand === 2)) {
-    result = '勝ちました。';
-  } else if ((com === GU && mainHand === 2) || (com === CHOKI && mainHand === 3) || (com === PA && mainHand === 1)) {
-    result = '負けました。';
-  } else {
-    result = '...'
   }
-
-
-
-
-
-
-
-
 
 
 
@@ -131,13 +88,8 @@ const Page2 = () => {
       <button onClick={choki}>ちょき</button>
       <button onClick={pa}>ぱー</button>
       <div>あなたの手:{hand}</div>
-      <div>CPUの手：{comHandName}</div>
+      <div>CPUの手：{cpuhand}</div>
       <div>結果は {result}</div>
-
-
-
-
-
 
     </div>
   )
